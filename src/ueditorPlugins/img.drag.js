f6e1490dfbd3aeee2ff21769764d876c6d3c515a
xiaozhen.quan <xiaozhen.quan@united-imaging.com>
2021-02-20 15:48:10 +0800

    鼠标样式

--- src/ueditorPlugins/img.drag.js
+++ src/ueditorPlugins/img.drag.js
@@ -1,13 +1,21 @@
+
 export default function installImgDragPlugin() {
   const utils = UE.utils
   const domUtils = UE.dom.domUtils
 
   UE.plugins["imgdrag"] = function() {
     const me = this
+    function mouseOverEvent(evt) {
+      me.body.style.cursor = "pointer"
+    }
+    function mouseOutEvent(evt) {
+      me.body.style.cursor = "text"
+    }
     me.addListener("contentchange", function() {
       const me = this
       utils.each(domUtils.getElementsByTagName(me.document, "img"), function(img) {
-        console.log(img)
+        domUtils.on(img, "mouseover", mouseOverEvent)
+        domUtils.on(img, "mouseout", mouseOutEvent)
       })
     })
   }
